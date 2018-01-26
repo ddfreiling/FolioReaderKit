58eacc7b9cf938319d46f0a8bf598e4b6d23d991
Daniel Freiling <dfg@nota.dk>
2018-01-26 13:32:59 +0100

    fix: FolioReaderConfig objc compatible

--- Source/FolioReaderConfig.swift
+++ Source/FolioReaderConfig.swift
@@ -44,7 +44,7 @@ import RealmSwift
 
  Eg. A ClassBasedOnClickListener with the className `quote` and parameterName `id` with the given epub html content `<section class="quote" id="12345">` would call the given closure on a click on this section with the String `12345` as parameter.
  */
-@objc public struct ClassBasedOnClickListener {
+public struct ClassBasedOnClickListener {
 
     /// The name of the URL scheme which should be used. Note: Make sure that the given `String` is a valid as scheme name.
     public var schemeName: String
